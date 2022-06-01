401221879a32e80708b44e37285fc6c038c0702b
Otávio Augusto <tavinteixeira@hotmail.com>
2022-05-31 21:52:37 -0300

    Ultimo lista 4

--- Lista4.c
+++ Lista4.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <locale.h>
 
 void ex6() {
     int num,fat=1,n1;
@@ -51,10 +53,10 @@ void ex9() {
     for (i = 2; i < 15; i++) {                                        
         soma = fib1 + fib2;  
 
-        fib1 = fib2;                           
-        fib2 = soma; 
+        fib2 = fib1;                           
+        fib1 = soma; 
 
-        printf(" %ds",soma);                          
+        printf(" %d",soma);                          
     } 
 
 }
@@ -69,6 +71,115 @@ void ex10() {
     }
 
     printf("%d", soma);
+}
+
+void ex12() {
+    char sexo;
+    int n_mulheres=0,n_homens=0;
+    float altura,max_homem=0,min_homem=0,max_mulher=0,min_mulher=0,soma_a_mulher=0;
+
+    for (int i=0; i<4; i++) {
+        do {
+            printf("Digite o sexo da pessoa: F ou M ");
+            fflush(stdin);
+            scanf("%c", &sexo);
+        } while (sexo != 'F' && sexo != 'M');
+
+        do {
+            printf("Digite a altura: ");
+            fflush(stdin);
+            scanf("%f",&altura);
+        } while (altura<=0 || altura>3);
+
+        if( sexo == 'F') {
+            if (n_mulheres == 0) {
+                max_mulher=altura;
+                min_mulher=altura;
+
+                soma_a_mulher=altura;
+            } else {
+                if (altura > max_mulher) {
+                    max_mulher=altura;
+                } 
+                if(altura < min_mulher) {
+                    min_mulher=altura;
+                }
+                soma_a_mulher = soma_a_mulher + altura;
+            }
+            n_mulheres++;
+        } else {
+            if (n_homens == 0) {
+                max_homem=altura;
+                min_homem=altura;
+            } else {
+                if (altura > max_homem) {
+                    max_homem=altura;
+                } 
+                if(altura < min_homem) {
+                    min_homem=altura;
+                }
+            }  
+            n_homens++;
+        }
+
+    }
+
+    printf("Maximo e minimo homens: Maior %f, Menor %f\n", max_homem, min_homem);
+    printf("Maximo e minimo mulheres: Maior %f, Menor %f\n", max_mulher, min_mulher);
+    printf("A media da altura das mulheres é: %f\n", soma_a_mulher/n_mulheres);
+    printf("Numero de homens %d, Porcentagem de homens %.2f", n_homens, (float)n_homens/(n_homens+n_mulheres)*100);
+}
+
+void ex14_Desafio() {
+    int cand1=0,cand2=0,cand3=0,cand4=0,nulo=0,branco=0,choice,total;
+    do {
+        system("clear");
+      	printf ("\n\n\t\t---Programa Eleição presidencial---");
+		printf("\n\n\t\t <1> Candidato 1");
+		printf("\n\t\t <2> Candidato 2");
+		printf("\n\t\t <3> Candidato 3");
+		printf("\n\t\t <4> Candidato 4");
+		printf("\n\t\t <5> Nulo");
+		printf("\n\t\t <6> Branco");
+		printf("\n\n\t\t <0> Encerrar");
+        do {
+            printf("\n\t Escolha uma opção: ");
+            scanf("%d", &choice);
+
+            if (choice > 6 || choice < 0 ) {
+                printf("\nEntrada invalida\ntente novamente:\n");
+            }
+
+        } while (choice > 6 || choice < 0);
+        
+
+        switch (choice) {
+            case 1: 
+                cand1++;
+                break;
+            case 2: 
+                cand2++;
+                break;
+            case 3: 
+                cand3++;
+                break;
+            case 4: 
+                cand4++;
+                break;
+            case 5: 
+                nulo++;
+                break;
+            case 6: 
+                branco++;
+                break;
+        }
+
+    } while (choice != 0);
+
+    total = cand1+cand2+cand3+cand4+nulo+branco;
+
+    printf("Total de votos de cand1: %d, cand2: %d, cand3: %d, cand4: %d, nulo: %d, branco: %d", cand1,cand2,cand3,cand4,nulo,branco);
+    printf("\nPercentual de nulos: %.2f%%, de brancos: %.2f%%", (float)nulo/total*100, (float)branco/total*100);
 }
 
 void ex15_DesafioA() {
@@ -159,8 +270,9 @@ void ex15_DesafioD() {
 }
 
 int main() {
+    setlocale(LC_ALL,"portuguese");
 
-    ex15_DesafioD();
+    ex12();
 
     return 0;
 }
